0f9145d2959da7e45f58029a26a857df3edbcb36
Abhishek Gupta <ravi37463@gmail.com>
2023-02-13 14:06:26 +0530

    edit-js

--- script.js
+++ script.js
@@ -16,24 +16,23 @@ const section4RightImage4 = "https://web-images.credcdn.in/_next/assets/images/h
 
 window.addEventListener('scroll'), () => {
     const scrollPosition = window.scrollY;
-    const section4OffsetTop = section4.offsetTop
+    const section4OffsetTop = section4.offsetTop;
     const section4Height = section4.clientHeight; //screen height
 
     if (( scrollPosition > section4OffsetTop) && (scrollPosition < section4Height + section4OffsetTop)){
 
         if(scrollPosition > section4OffsetTop && scrollPosition < section4OffsetTop + 300){
             section4Left1.style.opacity = 1;
-            section4Left1.style.tranition = "opacity 1s ease-in-out";
+            section4Left1.style.transition = "opacity 1s ease-in-out";
         } else if( scrollPosition > section4OffsetTop + innerHeight && scrollPosition < section4OffsetTop + innerHeight + 300){
-            section4Left1.style.opacity = 1;
-            section4Left1.style.tranition = "opacity 1s ease-in-out";
-        }
+            section4Left2.style.opacity = 1;
+            section4Left2.style.transition = "opacity 1s ease-in-out";
         } else if( scrollPosition > section4OffsetTop + innerHeight * 2 && scrollPosition < section4OffsetTop + innerHeight * 2 + 300){
-            section4Left1.style.opacity = 1;
-            section4Left1.style.tranition = "opacity 1s ease-in-out";
+            section4Left3.style.opacity = 1;
+            section4Left3.style.transition = "opacity 1s ease-in-out";
         } else if( scrollPosition > section4OffsetTop + innerHeight * 3 && scrollPosition < section4OffsetTop + innerHeight * 3 + 300){
-            section4Left1.style.opacity = 1;
-            section4Left1.style.tranition = "opacity 1s ease-in-out";
+            section4Left4.style.opacity = 1;
+            section4Left4.style.transition = "opacity 1s ease-in-out";
         } else {
             section4Left1.style.opacity = 0;
             section4Left2.style.opacity = 0;
@@ -54,7 +53,7 @@ window.addEventListener('scroll'), () => {
             section4RightImg.src = section4RightImage4;
             section4RightImg.style.transition = "opacity 1s ease-in-out";
         } else {
-            section4RightImg.src = section4RightImage1
+            section4RightImg.src = section4RightImage1;
         }
     }
 }
